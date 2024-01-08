05d6de6701e7ce66ff13c84d1834f85904b73d65
LofiTea <87150148+LofiTea@users.noreply.github.com>
2024-01-08 16:51:38 -0500

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -5,6 +5,7 @@ export default function Home() {
     return (
         <div>
             <Head>
+                <link rel='icon' href='./lofitea-portfolio/LofiTea.ico' />
                 <meta name="google-site-verification" content="VJs2G3ynvYQ1KG06ciOhdPdklC1gIUEkJ2KcplrnfWU" />
                 <meta name="description" content="Henry Lee's portfolio." />
                 <title>Home</title>
