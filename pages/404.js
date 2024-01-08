24fa795734f1189153956f41bcbd1c62b36b2052
LofiTea <87150148+LofiTea@users.noreply.github.com>
2024-01-08 09:48:56 -0500

    Update 404.js

--- pages/404.js
+++ pages/404.js
@@ -5,7 +5,7 @@ const Error404 = () => {
     return (
         <section className='flex items-center text-black'>
             <Head>
-                <link rel='icon' href='/public/LofiTea.ico?' />
+                <link rel='icon' href='../public/LofiTea.ico' />
                 <title>Error 404</title>
             </Head>
 
