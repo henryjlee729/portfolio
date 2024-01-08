3a181531418ec55533477599e6ae1716e2337724
LofiTea <87150148+LofiTea@users.noreply.github.com>
2024-01-08 17:48:47 -0500

    Update Home.js

--- components/Home.js
+++ components/Home.js
@@ -22,7 +22,7 @@ const Home = () => {
             }}
         >
             <Head>
-                <link rel='icon' href='./lofitea-portfolio/LofiTea.ico' />
+                <link rel='icon' href='./images/lofitea/LofiTea.jpg' />
             </Head>
             <div className='flex px-4 md:py-32 mx-auto h-screen items-center'>
                 <div className='text-center mx-auto'>
