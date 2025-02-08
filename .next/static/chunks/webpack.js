b59ce1ef72b8d181cd3fa2de8bc0e9b08f8822e9
LofiTea <iwilldiscoveraurora@gmail.com>
2025-02-08 01:32:09 -0500

    Fixed some bugs with images

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "901d9459c18b9572"; }
+/******/ 		__webpack_require__.h = function() { return "68412a8d15b26c11"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- .next/static/webpack/webpack.ae5d6ce9726105b7.hot-update.js
+++ .next/static/webpack/webpack.ae5d6ce9726105b7.hot-update.js
@@ -11,7 +11,7 @@ self["webpackHotUpdate_N_E"]("webpack",{},
 /******/ function(__webpack_require__) { // webpackRuntimeModules
 /******/ /* webpack/runtime/getFullHash */
 /******/ !function() {
-/******/ 	__webpack_require__.h = function() { return "8608036f0f3c2299"; }
+/******/ 	__webpack_require__.h = function() { return "754591e4307daaff"; }
 /******/ }();
 /******/ 
 /******/ }
